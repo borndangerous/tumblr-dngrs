3a75ac1279407e1abb43c9e68bf6f2d93c30b8ba
David Walsh <dvdwlsh@gmail.com>
2016-01-03 20:45:12 -0500

    Tweak

--- app.js
+++ app.js
@@ -2,4 +2,15 @@
 
 $(document).ready(function() {
   console.log("DNGRS > Ready");
+
+  $(".convert-emoji").each(function() {
+    var original = $(this).html();
+
+    //var converted = emojione.toImage(original);
+    var converted = emojione.shortnameToImage(original);
+    
+    $(this).html(converted);
+
+  });
+
 });
